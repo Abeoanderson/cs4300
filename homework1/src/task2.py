b6e669d0bdc3fc406b7afafba2a650ffe4fb230b
Abe Anderson <your_email@example.com>
2026-02-06 02:58:30 +0000

    task 2 mostly done

---
+++ homework1/src/task2.py
@@ -0,0 +1,10 @@
+def main():
+    x = 1 # int
+    y = 2.0  # float
+    z = "abc"  # string
+    a = True    # bool
+
+
+
+if __name__ == "__main__":
+    main()
--- homework1/tests/test_task1.py
+++ homework1/tests/test_task1.py
@@ -1,2 +1,6 @@
+from src.task1 import main
+
 def test_task1(capsys):
+    main()
+    captured = capsys.readouterr()
     assert captured.out == "Hello, World!\n"
