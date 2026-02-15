74914c089a9e3592f235c9c875229df55b344b91
Abe Anderson <your_email@example.com>
2026-02-15 21:51:07 +0000

    all tasks and test done

--- homework1/src/task6.py
+++ homework1/src/task6.py
@@ -1,15 +1,17 @@
 import string
 
-def main():
+def count_num_words():
+    """opens task6_read_me and counts number of words"""
     with open("task6_read_me.txt","r") as f:
         text = f.read()
-
+    #it was counting the puncuation as words so we map " " to all found punctuation
     for p in string.punctuation:
         text = text.replace(p," ")
 
     f.close()
-
-    return len(text.split())
+    #split into strings based on " " as a defualt val and get length of list
+    count = len(text.split())
+    return count
 
 if __name__ == "__main__":
-    main()
+    count_num_words()
--- homework1/tests/test_task6.py
+++ homework1/tests/test_task6.py
@@ -1,5 +1,5 @@
-from src.task6 import main
+from src.task6 import count_num_lines
 
 def test_word_count(capsys):
-    word_count = main()
+    word_count = count_num_lines()
     assert word_count == 104
