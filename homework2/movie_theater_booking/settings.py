f5f950e8e2421aa37810bec3690f7488def193e9
Abe Anderson <your_email@example.com>
2026-03-03 18:15:07 +0000

    update settings.py for render deploy

--- homework2/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/settings.py
@@ -80,11 +80,15 @@ WSGI_APPLICATION = 'movie_theater_booking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600,
-        )
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
 
 
